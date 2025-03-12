971d1977625022202ef4cf8f04367d41a68ed36a
Tim Potze <timpotze@gmail.com>
2025-03-12 23:53:10 +0100

    Initial commit

---
+++ ManagedLibrary/ManagedWorker.cs
@@ -0,0 +1,34 @@
+﻿using ManagedLibrary;
+using System;
+using System.Reflection.Metadata;
+using System.Runtime.InteropServices;
+
+[assembly: MetadataUpdateHandler(typeof(ManagedWorker))]
+namespace ManagedLibrary
+{
+    public class ManagedWorker
+    {
+        public static void ClearCache(Type[]? updatedTypes)
+        {
+            Console.WriteLine("ClearCache");
+        }
+
+        public static void UpdateApplication(Type[]? updatedTypes)
+        {
+            Console.WriteLine("UpdateApplication");
+        }
+
+        private static int _counter;
+
+        [UnmanagedCallersOnly]
+        public static void DoWork()
+        {
+            Console.WriteLine($"working! {_counter++}");
+            // Console.WriteLine("changes made");
+        }
+
+        public static void Main()
+        {
+        }
+    }
+}
